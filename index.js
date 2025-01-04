5dd94040008012709378a99e2f04984033fb6a45
Soseh <sosehv@gmail.com>
2025-01-04 14:10:55 -0500

    Replaces data argument with coordinates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/*const { fetchMyIP } = require('./iss');
-
+const { fetchCoordsByIP } = require('./iss');
+/*
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -9,3 +9,11 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:', ip);
 }); */
 
+fetchCoordsByIP('174.114.143.160',(error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned coordinates:", coordinates);
+});
