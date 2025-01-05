fc7e476d7494780d5eda6d6e4c6ed2cb565a37f5
Soseh <sosehv@gmail.com>
2025-01-05 18:19:01 -0500

    Adds printPassTimes function and calls it

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+let timestamp = 1736147047;
+let date = new Date(timestamp * 1000); // multiply by 1000 to convert to milliseconds
+console.log(date.toString());
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // Success, print out the dates!
-  console.log(passTimes);
+  printPassTimes(passTimes);
 });
