c6f9bacba9f2a119ba4de2f318da30d7366c72d4
Soseh <sosehv@gmail.com>
2025-01-05 13:25:34 -0500

    Replaces response argument with passTimes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+//const exampleCoords = { latitude: '45.4215296', longitude: '-75.6971931' };
+const exampleCoords = { latitude: 45.4215296, longitude: -75.6971931 };
 /*
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,7 +9,7 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:', ip);
-}); */
+}); 
 
 fetchCoordsByIP('174.114.143.160',(error, coordinates) => {
   if (error) {
@@ -16,4 +18,12 @@ fetchCoordsByIP('174.114.143.160',(error, coordinates) => {
   }
 
   console.log("It worked! Returned coordinates:", coordinates);
+}); */
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned array:", passTimes);
 });
