0b4b111974488bb2c4c823ce540948eb03df77c0
Soseh <sosehv@gmail.com>
2025-01-05 14:16:17 -0500

    Removes unnecessary code and adds a call to nextISSTimesForMyLocation function

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-const { fetchISSFlyOverTimes } = require('./iss');
-//const exampleCoords = { latitude: '45.4215296', longitude: '-75.6971931' };
-const exampleCoords = { latitude: 45.4215296, longitude: -75.6971931 };
-/*
-fetchMyIP((error, ip) => {
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned IP:', ip);
-});
-
-fetchCoordsByIP('174.114.143.160',(error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log("It worked! Returned coordinates:", coordinates);
-}); */
-
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-  console.log("It worked! Returned array:", passTimes);
+  // Success, print out the dates!
+  console.log(passTimes);
 });
