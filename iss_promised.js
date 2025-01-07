f2bb6e24dc305005896747182519004e0114b330
Soseh <sosehv@gmail.com>
2025-01-07 12:46:49 -0500

    Fixes linting issues

--- iss_promised.js
+++ iss_promised.js
@@ -2,27 +2,27 @@ const needle = require('needle');
 
 const fetchMyIP = function() {
   return needle('get','https://api.ipify.org?format=json')
-  .then((response) => {
-    const body = response.body; // retrieve the body value from the response object
-    const ip = body.ip; // retrieve the ip from the body object
-    return ip;
-  });
+    .then((response) => {
+      const body = response.body; // retrieve the body value from the response object
+      const ip = body.ip; // retrieve the ip from the body object
+      return ip;
+    });
 };
 
-/* 
+/*
  * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
  * Input: IP address as a string
  * Returns: Promise of request for lat/lon
  */
 const fetchCoordsByIP = function(ip) {
   return needle('get', `http://ipwho.is/${ip}`)
-  .then((response) => {
-    const body = response.body; //retrieve body from response
-    const latitude = body.latitude; // retrieve latitude from body
-    const longitude = body.longitude; //retrieve longitude from body
-    return {latitude, longitude};
+    .then((response) => {
+      const body = response.body; //retrieve body from response
+      const latitude = body.latitude; // retrieve latitude from body
+      const longitude = body.longitude; //retrieve longitude from body
+      return {latitude, longitude};
 
-  });
+    });
 };
 
 /*
@@ -31,18 +31,18 @@ const fetchCoordsByIP = function(ip) {
  * Returns: Promise of request for fly over data, returned as JSON string
  */
 const fetchISSFlyOverTimes = function(coords) {
-  const latitude = coords.latitude
+  const latitude = coords.latitude;
   const longitude = coords.longitude;
   const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
   return needle('get', url)
-  .then((response) => {
-    const body = response.body;
-    const passtimes = body.response; // changed the name from response to passtimes for clarification
-    return passtimes;
-  });
+    .then((response) => {
+      const body = response.body;
+      const passtimes = body.response; // changed the name from response to passtimes for clarification
+      return passtimes;
+    });
 };
 
- /* 
+/*
  * Input: None
  * Returns: Promise for fly over data for users location
  */
